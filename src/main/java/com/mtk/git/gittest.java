27befa3d89ccc3aa92392b1af4c4e6bb3edad6ab
Pzz <2320639341@qq.com>
2021-07-28 15:12:30 +0800

    02 commit

--- src/main/java/com/mtk/git/gittest.java
+++ src/main/java/com/mtk/git/gittest.java
@@ -8,14 +8,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("hi.,git");
 
-        System.out.println("hi.git");
-
-        System.out.println("hi,git");
-
-        System.out.println("hi,git");
-        System.out.println("hi,git");
-
-        System.out.println("hi,git");
-        System.out.println("hi,git");
+        System.out.println("hi,github");
     }
 }
